a18d105b182eab42312df532b5c570457bcbbda0
Eric Saxby <sax@ericsaxby.com>
2010-02-04 10:28:07 -0800

    adding exceptional to project

--- img2png.rb
+++ img2png.rb
@@ -6,12 +6,33 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 require 'models/png'
+require 'exceptional'
 
+Exceptional.api_key = '8ebb510cbd6126e092625d58912121dd848e3a2e'
+
+module Exceptional
+  def self.handle_sinatra(exception, uri, request, params)
+    e = Exceptional.parse(exception)
+
+    e.framework = "sinatra"
+    e.controller_name = uri
+    e.occurred_at = Time.now.strftime("%Y%m%d %H:%M:%S %Z")
+    e.environment = request.env.to_hash
+    e.url = uri
+    e.parameters = params.to_hash
+
+    Exceptional.post e
+  end
+end
 
 class Controller < Sinatra::Base
 
   # use Rack::Static, :urls => ['/css', '/js', '/images', '/about'], :root => 'public'
   mime 'png', 'image/png'
+
+  error do
+    Exceptional.handle_sinatra(request.env['sinatra.error'], request.env['REQUEST_URI'], request, params)
+  end
 
   get "/" do
     erb :index
