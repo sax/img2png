ce03eb1e11e974cd809508d59987b6deefc92671
Eric Saxby <sax@ericsaxby.com>
2010-02-04 15:11:19 -0800

    removing exceptional

--- img2png.rb
+++ img2png.rb
@@ -18,7 +18,7 @@ class Controller < Sinatra::Base
   
   get "/*/i.png" do
     content_type 'image/png'
-    png = Img2Png::Png.new(:src => params[:splat].to_s.gsub!(/(http)\//, '\1://'))
+    png = Img2Png::Png.new(:src => params[:splat].to_s.gsub!(/(https?)\//, '\1://'))
     png.reformat
     png.to_blob
     # send_file png.to_blob, :type => 'image/png', :disposition => 'inline'
